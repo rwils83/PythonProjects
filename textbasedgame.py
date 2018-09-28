b51f4b512d00e5249f273fdfde14e9b0a383af15
Ryan Wilson <rwils83@gmail.com>
2018-09-28 09:33:39 -0400

    started a text based game

---
+++ textbasedgame.py
@@ -0,0 +1,16 @@
+### This is a simple text based RPG made in Python by
+### Ryan J. Wilson
+print("The year is 2222. I don't know why I chose that year, but I did.")
+print("This game is based on you getting your ass kicked by some kind of")
+print("monster. But I am too lazy to write the story line right this ")
+print("second. So I am just putting a bunch of placeholder words right")
+print("here for now. Because I hate writing the actual story line for")
+print("a game. Note to self. Find someone to write the story lines for")
+print(" all your games. End note")
+
+playerName=raw_input("What is your name player? ")
+characterName =raw_input("What would you like to name your character? ")
+
+print(playerName)
+print(characterName)
+
